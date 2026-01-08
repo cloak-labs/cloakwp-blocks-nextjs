e4e04b9bc2cc819cf93bb9b3bb2d95451362651d
Kaelan Smith <kaelancsmith@gmail.com>
2026-01-08 11:48:21 -0800

    rebuild dist

--- dist/blocks/image.d.ts
+++ dist/blocks/image.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="react" />
 import { type ImageProps } from "@cloakui/nextjs-primitives";
 export declare const image: (userOverrides?: {
-    dataRouter?: import("cloakwp/blocks").WPDataRouter<import("@cloakui/types").TImageProps>;
+    dataRouter?: import("cloakwp/blocks/types").WPDataRouter<import("@cloakui/types").TImageProps>;
     component?: import("react").ComponentType<ImageProps>;
+    meta?: {
+        [x: string]: any;
+    };
 }) => import("@cloakwp/react").WPBlocksConfigReact;
